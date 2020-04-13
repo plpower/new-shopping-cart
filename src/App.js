85eccc7546093a77a19dc90ff9085376a25ec0dd
Patrice Power <patricepower2021@u.northwestern.edu>
2020-04-12 20:46:47 -0400

    Added firebase and public site

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4-p223h9-HoXWrzr7R1Jc0iZ1dV3YnfU",
+  authDomain: "new-shopping-cart-f5a66.firebaseapp.com",
+  databaseURL: "https://new-shopping-cart-f5a66.firebaseio.com",
+  projectId: "new-shopping-cart-f5a66",
+  storageBucket: "new-shopping-cart-f5a66.appspot.com",
+  messagingSenderId: "904348819660",
+  appId: "1:904348819660:web:2da5422b04fbf633212269",
+  measurementId: "G-FWSGT8YB77"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
