5295488e2aa260227ec17c216ecb2d69e5cc3854
Patrice Power <patricepower2021@u.northwestern.edu>
2020-04-19 00:47:49 -0400

    Static shopping cart and state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Grid , makeStyles, Box } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+
 import './App.css';
 import ProductCard from './components/ProductCard.js';
 
@@ -26,6 +30,7 @@ const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
   const productids = Object.keys(data);
+
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -37,8 +42,18 @@ const App = () => {
 
   const [spacing, setSpacing] = React.useState(2);
 
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setVisible(open);
+  };
+
+  const [visible, setVisible] = React.useState(false);
+
   return (
-      
+    <div>
+    <Button onClick={toggleDrawer(true)}>My Cart</Button>
     <Grid container justify="center" spacing={spacing}>
       {products.map((product, index) => (
         <Grid key={product.sku} item>
@@ -47,6 +62,15 @@ const App = () => {
         </Grid>
       ))}
     </Grid>
+    <Drawer anchor="right" open={visible} onClose={toggleDrawer(false)}>
+        {products.map((product, index) => (
+          <Grid key={product.sku} item>
+            <ProductCard product={product} productid={productids[index]} className={product.title}>
+            </ProductCard>
+          </Grid>
+        ))}
+    </Drawer>
+    </div>
   );
 };
 
